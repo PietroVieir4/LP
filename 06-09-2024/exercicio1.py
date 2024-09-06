d59a1576ee2393a5bd1668b94471c922c50ab28b
Castrosama <161356389+Castrosama@users.noreply.github.com>
2024-09-06 14:12:23 -0300

    fdgh

--- 06-09-2024/exercicio1.py
+++ 06-09-2024/exercicio1.py
@@ -4,15 +4,15 @@ lista1=[]
 lista2=[]
 lista3=[]
 
-for i in range (2):
+for i in range (3):
     user1=int(input(f"insira o {i+1}° numero"))
     lista1.append(user1)
-    for j in range (2):
-        user2=int(input(f"insira o {j+1}° numero"))
-        lista2.append(user2)
-        for f in range (2):
-            user3=int(input(f"insira o {f+1}° numero"))
-            lista3.append(user3)
+for j in range (3):
+    user2=int(input(f"insira o {j+1}° numero"))
+    lista2.append(user2)
+for f in range (3):
+    user3=int(input(f"insira o {f+1}° numero"))
+    lista3.append(user3)
 
 matriz=[lista1, lista2, lista3]
 
