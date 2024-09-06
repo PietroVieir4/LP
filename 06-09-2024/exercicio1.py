cfb975306da82d55cf66f88ec7ceaefadac21028
Castrosama <161356389+Castrosama@users.noreply.github.com>
2024-09-06 14:30:05 -0300

    hgjf

--- 06-09-2024/exercicio1.py
+++ 06-09-2024/exercicio1.py
@@ -8,10 +8,10 @@ for i in range (3):
     user1=int(input(f"insira o {i+1}° numero"))
     lista1.append(user1)
 for j in range (3):
-    user2=int(input(f"insira o {j+1}° numero"))
+    user2=int(input(f"insira o {j+4}° numero"))
     lista2.append(user2)
 for f in range (3):
-    user3=int(input(f"insira o {f+1}° numero"))
+    user3=int(input(f"insira o {f+7}° numero"))
     lista3.append(user3)
 
 matriz=[lista1, lista2, lista3]
--- 06-09-2024/exercicio2.py
+++ 06-09-2024/exercicio2.py
@@ -1,8 +1,26 @@
 '''Vinicius Pietro V. Rafael A.'''
 
-lista1=int(input("insira uma  lista de palavras, e separe cada uma com espaço: ")).split()
-lista2=int(input("insira uma  lista de palavras, e separe cada uma com espaço: ")).split()
-lista3=int(input("insira uma  lista de palavras, e separe cada uma com espaço: ")).split()
+lista1=[]
+lista2=[]
+lista3=[]
 
 matriz=[lista1, lista2, lista3]
-print(matriz)
+
+for coluna in matriz:
+    for elemento in coluna:
+        for i in range (3):
+            user1=int(input(f"insira o {i+1}° numero"))
+            lista1.append(user1)
+            if elemento<0:
+                print(f"o {i+1} é negativo")
+        for j in range (3):
+            user2=int(input(f"insira o {j+1}° numero"))
+            lista2.append(user2)
+            if elemento<0:
+                print(f"o {j+1} é negativo")
+        for f in range (3):
+            user3=int(input(f"insira o {f+1}° numero"))
+            lista3.append(user3)
+            if elemento<0:
+                print(f"o {i+1} é negativo")
+
